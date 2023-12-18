19536cb9c2c7762d65bd238d57083c49ec27b490
rezahafis <142420173+rezahafis@users.noreply.github.com>
2023-12-18 22:53:31 +0700

    Update service.js

--- controllers/service.js
+++ controllers/service.js
@@ -1,7 +1,6 @@
 import Laundrys from "../models/laundryModel.js";
 import Layanan from "../models/layananModel.js";
 
-// Create a new laundry service for a specific laundry
 export const createService = async (req, res) => {
   const laundryId = req.laundry.laundryID;
   const { namaLayanan, harga } = req.body;
@@ -10,7 +9,8 @@ export const createService = async (req, res) => {
     // Validate input
     if (!namaLayanan || !harga) {
       return res.status(400).json({
-        error: true,
+        success: false,
+        statusCode: 400,
         message: "Invalid input data",
       });
     }
@@ -19,23 +19,19 @@ export const createService = async (req, res) => {
 
     if (!laundry) {
       return res.status(404).json({
-        error: true,
+        success: false,
+        statusCode: 404,
         message: "Laundry Tidak Ditemukan",
       });
     }
 
     // Update the database record
-<<<<<<< HEAD
     const dataLayanan = await Layanan.create({
-=======
-    const createdLayanan = await Layanan.create({
->>>>>>> c553798674b0c0758b396d5b217a34efacaa2ae2
       name: namaLayanan,
       id_laundry: laundryId,
       harga: harga,
     });
 
-<<<<<<< HEAD
     const responseData = {
       error: false,
       message: "Layanan laundri berhasil dibuat",
@@ -51,38 +47,18 @@ export const createService = async (req, res) => {
   };
 
     res.json(responseData);
-=======
-    res.status(201).json({
-      error: false,
-      message: "Layanan laundri berhasil dibuat",
-      resultLayanan: {
-        id: createdLayanan.id,
-        namaLayanan: createdLayanan.name,
-        hargaLayanan: createdLayanan.harga,
-        status: "Tidak Tersedia", // Assuming you want to set a default status
-        createdAt: createdLayanan.createdAt,
-        updatedAt: createdLayanan.updatedAt,
-        laundryId: createdLayanan.id_laundry,
-      },
-    });
->>>>>>> c553798674b0c0758b396d5b217a34efacaa2ae2
   } catch (error) {
     console.error("Error creating service:", error);
     res.status(500).json({
       error: true,
-<<<<<<< HEAD
       statusCode: 500,
       error: {
         message: "Internal Server Error",
       },
-=======
-      message: "Gagal membuat layanan",
->>>>>>> c553798674b0c0758b396d5b217a34efacaa2ae2
     });
   }
 };
 
-<<<<<<< HEAD
 export const getAllServices = async (req, res) => {
   const laundryId = req.laundry.laundryID;
 
@@ -267,72 +243,10 @@ export const deleteService = async (req, res) => {
     };
 
     res.json(responseData);
-=======
-// Edit details of a specific laundry service
-export const editService = async (req, res) => {
-  const { id } = req.params;
-  const { namaLayanan, harga } = req.body;
-
-  try {
-    const layanan = await Layanan.findByPk(id);
-    if (!layanan) {
-      return res.status(404).json({
-        error: true,
-        message: `Layanan #${id} tidak ditemukan`,
-      });
-    }
-    
-    await layanan.update({
-      name: namaLayanan,
-      harga,
-    });
-
-    res.status(200).json({
-      error: false,
-      message: "Layanan laundri berhasil dirubah",
-      resultLayanan: {
-        id: layanan.id,
-        namaLayanan: layanan.name,
-        hargaLayanan: layanan.harga,
-        status: layanan.status,
-      },
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      error: true,
-      message: "Gagal merubah layanan",
-    });
-  }
-};
-
-// Delete a specific laundry service
-export const deleteService = async (req, res) => {
-  const { id } = req.params;
-
-  try {
-    const layanan = await Layanan.findByPk(id);
-
-    if (!layanan) {
-      return res.status(404).json({
-        error: true,
-        message: `Layanan #${id} tidak ditemukan`,
-      });
-    }
-
-    // Delete the record
-    await layanan.destroy();
-
-    res.status(200).json({
-      error: false,
-      message: "Layanan laundri berhasil dihapus",
-    });
->>>>>>> c553798674b0c0758b396d5b217a34efacaa2ae2
   } catch (error) {
     console.error("Error deleting service:", error);
     res.status(500).json({
       error: true,
-<<<<<<< HEAD
       statusCode: 500,
       error: {
         message: "Internal Server Error",
@@ -340,9 +254,3 @@ export const deleteService = async (req, res) => {
     });
   }
 };
-=======
-      message: "Gagal menghapus layanan",
-    });
-  }
-};
->>>>>>> c553798674b0c0758b396d5b217a34efacaa2ae2
